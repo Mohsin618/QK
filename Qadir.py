d5741a1b5ec302b325db8c849f857b7eb5c07a5f
Mohsin618 <103148832+Mohsin618@users.noreply.github.com>
2022-05-02 23:05:56 +0500

    Add files via upload

---
+++ Qadir.py
@@ -0,0 +1,16 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Qadir import knock_Out
+    knock_Out()
+elif bit == '32bit':
+    from Mahar import knock_Out
+    knock_Out()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.sys.exit()
